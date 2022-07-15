a9dc3aa4d3c27a870430efda32786e4a48fd4074
Umk1nus <umk1namw@gmail.com>
2022-07-15 20:07:14 +0300

    start proj

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Header from '/src/components/Header.vue'
+import Footer from '/src/components/Footer.vue'
+
+export default {
+  components: {Header, Footer},
+    data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <Header/>
+  <Footer/>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="p-4 sm:p-6 bg-gray-900">
+    <div class="sm:flex sm:items-center sm:justify-center">
+        <span class="text-sm text-gray-400 sm:text-center">
+          © {{new Date().getFullYear()}} <a href="https://github.com/Umk1nus" class="hover:underline">Umk1nus</a>.
+        </span>
+    </div>
+  </footer>
+</template>
